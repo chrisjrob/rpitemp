888066d40bd73887ec1d3deecc4b03aa091248b7
chrisjrob <chrisjrob@gmail.com>
2017-07-22 17:41:59 +0100

    move to weekly graphs

--- bin/generate-graphs.pl
+++ bin/generate-graphs.pl
@@ -5,51 +5,64 @@
 use strict;
 use warnings;
 
-my $filename = '../readings-extract.json';
+my $filename = 'readings-valid.json';
 
 if (not -e $filename) {
     die "$filename does not exist";
 }
 
-my @data = get_graph_data( $filename );
+my $data = get_graph_data( $filename );
 
-use GD::Graph::lines;
-
-my $graph = GD::Graph::lines->new(800, 600);
-$graph->set( 
-    title               => 'Server Room Temperature',
-    bgclr               => 'white',
-    transparent         => 0,
-    x_label             => 'Time',
-    y_label             => 'Temperature',
-    x_labels_vertical   => 1,
-) or die $graph->error;
-
-my $gd = $graph->plot(\@data) or die $graph->error;
-
-open(IMG, '>file.png') or die $!;
-binmode IMG;
-print IMG $gd->png;
+foreach my $week (sort keys %{ $data }) {
+    produce_graph_for_week($week, $data->{ $week });
+}
 
 exit;
+
+sub produce_graph_for_week {
+    my ($week, $data) = @_;
+
+    use GD::Graph::lines;
+
+    my $graph = GD::Graph::lines->new(800, 400);
+    $graph->set( 
+        title               => "Server Room Temperature - Week $week",
+        bgclr               => 'white',
+        transparent         => 0,
+        x_label             => 'Time',
+        x_labels_vertical   => 1,
+        x_label_skip        => 24,
+        x_last_label_skip   => 1,
+        y_label             => 'Temperature',
+    ) or die $graph->error;
+
+    my $gd = $graph->plot($data) or die $graph->error;
+
+    open(IMG, '>', "graphs/$week.png") or die $!;
+    binmode IMG;
+    print IMG $gd->png;
+
+}
 
 sub get_graph_data {
     my $filename = shift;
 
     my $history_ref = load_json_from_file( $filename );
 
-    my @data;
+    my $data;
     foreach my $timestamp (sort keys %{ $history_ref } ) {
-        my $date = date_from_epoch($timestamp);
-        push(@{ $data[0] }, $date);
-        my $index = 1;
+        my ($week, $date) = date_from_epoch($timestamp);
+        push(@{ $data->{ $week }[0] }, $date);
+        push(@{ $data->{ $week }[1] }, 30);
+        push(@{ $data->{ $week }[2] }, 28);
+        my $index = 3;
         foreach my $sensor (sort keys %{ $history_ref->{ $timestamp } }) {
-            push(@{ $data[ $index ] }, $history_ref->{ $timestamp }{ $sensor });
+            push(@{ $data->{ $week }[ $index ] }, $history_ref->{ $timestamp }{ $sensor });
             $index++;
         }
     }
 
-    return @data;
+    return $data;
 }
 
 sub date_from_epoch {
@@ -59,7 +72,10 @@ sub date_from_epoch {
 
     my $dt = DateTime->from_epoch( epoch => $epoch );
 
-    return $dt->ymd('-') . ' ' . substr($dt->hms(':'),0,5);
+    return(
+        $dt->year() . '-' . $dt->week(),
+        $dt->ymd('-') . ' ' . substr($dt->hms(':'),0,5)
+    );
 }
 
 sub load_json_from_file {
